31341f97d5ec7aba52210fb17a7f216ba8cc5ef7
Prachur Gupta <prachur12@gmail.com>
2023-05-13 18:51:11 +0530

    Made basic templates for allsections.APInotfetched

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import {
+  Paper,
+  Typography,
+  useMediaQuery,
+} from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMoobile = useMediaQuery(
+    "(min-width:600px)"
+  );
+  const coordinates = {
+    lat: 28.7041,
+    lng: 77.1025,
+  };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: "AIzaSyCsOknwVnGTa0Hp8Lb3AKdO20ZIcCUrV_4",
+        }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={10}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
